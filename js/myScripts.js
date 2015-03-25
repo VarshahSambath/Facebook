ef455b54be2b3f13ae1c8dd91cc1d01b1a0aff5b
Varshah <varshah@qburst.com>
2015-03-25 18:06:06 +0530

    fixed alignment issues and added js

---
+++ js/myScripts.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+$( document ).ready(function() {
+	
+	$('#postBottomBar').click(function(){//for the post status bottombar toshow when clicked on the text area
+		$("#show1").show();
+	});
+});
